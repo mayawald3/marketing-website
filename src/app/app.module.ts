7bd24b2c1bac0c8c8f2d84e0992366c2b06895b0
Maya <mayawald3@gmail.com>
2023-10-06 14:25:02 -0500

    MW1 - Adding all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {LandingPageComponent} from "./components/main-portlet/landing-page/landing-page.component";
+import {MainPortletComponent} from "./components/main-portlet/main-portlet.component";
+import {ServicesComponent} from "./components/main-portlet/services/services.component";
+import {ContactComponent} from "./components/main-portlet/contact/contact.component";
+import {WorkHistoryComponent} from "./components/main-portlet/work-history/work-history.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPortletComponent,
+    LandingPageComponent,
+    ContactComponent,
+    ServicesComponent,
+    WorkHistoryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/main-portlet/main-portlet.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'main-portlet',
+  templateUrl: './main-portlet.component.html',
+  styleUrls: ['./main-portlet.component.css']
+})
+export class MainPortletComponent {
+  title = 'main-portlet'
+}
